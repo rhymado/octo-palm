c34651ba752331bade49acc25a799427ac52d759
rhymaado <mightydhiyamatra@yahoo.co.id>
2023-03-15 11:28:10 +0700

    basic component, state dan props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class NamaKomponen extends Component {//implementasi komponen}
+class App extends Component {
+  // state merupakan variabel lokal dari class (object)
+  // jika state berubah maka komponen diperbaharui dom nya (rerender)
+  // untuk mengubah state gunakan method setState
+  state = {
+    counter: 0,
+    address: "Jakarta",
+  };
+  changeCounter = () => {
+    this.setState({
+      counter: 2,
+    });
+    // this.state.counter = 2;
+  };
+  render() {
+    // console.log(this.state.counter);
+    // console.log(this.props);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a className="App-link" href={this.props.href} target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+          <div>
+            <p>Counter: {this.state.counter}</p>
+            <button onClick={this.changeCounter}>Change Counter</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
